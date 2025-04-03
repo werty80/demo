923450dbeae59a53ec36feec53cb41a0d795aee5
Sayim <saayim.veem@gmail.com>
2025-04-03 14:29:43 +0500

    Initial commit

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -13,7 +13,7 @@ class HouseController extends Controller
      */
     public function index()
     {
-        //
+        return view('houses.index');
     }
 
     /**
---
+++ resources/views/houses/index.blade.php
@@ -0,0 +1,20 @@
+<form action="{{ route('houses.store') }}" method="POST">
+    @csrf
+
+    <div>
+        <label for="village">Select Village:</label>
+        <select name="village_id" id="village" required>
+            <option value="">-- Select a Village --</option>
+            @foreach($villages as $village)
+                <option value="{{ $village->id }}">{{ $village->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div>
+        <label for="name">House Name:</label>
+        <input type="text" name="name" id="name" required />
+    </div>
+
+    <button type="submit">Add House</button>
+</form>

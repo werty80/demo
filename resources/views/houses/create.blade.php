98ae81fb545d957edd6d16124d6b7acc6f43fb16
Sayim <saayim.veem@gmail.com>
2025-04-06 14:19:31 +0500

    changes v3

--- resources/views/houses/create.blade.php
+++ resources/views/houses/create.blade.php
@@ -30,8 +30,7 @@
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <a href="/islands"
                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >Cancel</a href="/islands"
-                  >
+                >Cancel</a>
                 <button type="submit"
                         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Save

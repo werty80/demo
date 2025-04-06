2103b02f85f5cf8ce7fdff98cfe428bdbb37e3a8
Sayim <saayim.veem@gmail.com>
2025-04-06 11:26:16 +0500

    code reformated

--- resources/views/village/show.blade.php
+++ resources/views/village/show.blade.php
@@ -64,7 +64,8 @@
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-red-600 hover:text-red-900"
-                                        onclick="return confirm('Are you sure you want to delete this house?');">Delete
+                                        onclick="return confirm('Are you sure you want to delete this employer?');">
+                                    Delete
                                 </button>
                             </form>
                         @endif

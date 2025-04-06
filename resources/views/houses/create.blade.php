03e58532fc9c99fbaf59db1e8d7a9a523b1f802c
Sayim <saayim.veem@gmail.com>
2025-04-06 13:59:38 +0500

    changes v3

--- resources/views/houses/create.blade.php
+++ resources/views/houses/create.blade.php
@@ -1,22 +1,41 @@
 <x-layout>
-    <form action="{{ route('houses.store') }}" method="POST">
+    <x-slot:heading>
+        Create Job
+    </x-slot:heading>
+
+    <form method="POST" action="/houses">
         @csrf
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="text"
+                                       name="name"
+                                       id="name"
+                                       class="block min-w-0 grow py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        required>
+                            </div>
+                            @error('name')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        <div>
-            <label for="village">Select Village:</label>
-            <select name="village_id" id="village" required>
-                <option value="">-- Select a Village --</option>
-                @foreach($villages as $village)
-                    <option value="{{ $village->id }}">{{ $village->name }}</option>
-                @endforeach
-            </select>
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <a href="/employers"
+                   class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >Cancel</a>
+                <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Save
+                </button>
+            </div>
         </div>
-
-        <div>
-            <label for="name">House Name:</label>
-            <input type="text" name="name" id="name" required />
-        </div>
-
-        <button type="submit">Add House</button>
     </form>
 </x-layout>

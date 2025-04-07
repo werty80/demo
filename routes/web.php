865eeb19d580cb2dd7dbee51aa53dfdbcc3f62c0
Sayim <saayim.veem@gmail.com>
2025-04-07 10:58:05 +0500

    Remove unused route for viewing individual houses

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,3 @@ Route::post('/logout', [SessionController::class, 'destroy']);
 Route::get('/contacts', [ContactController::class, 'index']);
 Route::get('/houses/create', [HouseController::class, 'create'])->name('houses.create');
 Route::post('/houses', [HouseController::class, 'store'])->name('houses.store');
-Route::get('/houses/{id}', [HouseController::class, 'show'])->name('houses.show');

a3d1e0047c41ab0aa80b1653dc16dccbf40ba3c1
Sayim <saayim.veem@gmail.com>
2025-04-07 10:28:17 +0500

    Add input field for village_id in house creation form

--- resources/views/houses/create.blade.php
+++ resources/views/houses/create.blade.php
@@ -41,6 +41,26 @@
                     </div>
                 </div>
             </div>
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="village_id" class="block text-sm/6 font-medium text-gray-900">Village Id</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="text"
+                                       name="village_id"
+                                       id="village_id"
+                                       class="block min-w-0 grow py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        required>
+                            </div>
+                            @error('village_id')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="submit"

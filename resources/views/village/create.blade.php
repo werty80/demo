fce1e0cd4954343705cad642e5b3ce49268460f4
Sayim <saayim.veem@gmail.com>
2025-04-09 10:56:40 +0500

    Remove unnecessary blank line in code input markup

--- resources/views/village/create.blade.php
+++ resources/views/village/create.blade.php
@@ -69,7 +69,6 @@
                                        name="code"
                                        id="code"
                                        value="{{ old('code') }}"
-
                                        class="block min-w-0 grow py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
                                        required>
                             </div>

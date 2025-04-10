136d1c7a771f5cdc60ab2be3cda924e6148c4e6d
Sayim <saayim.veem@gmail.com>
2025-04-10 14:32:17 +0500

    Add index method to PeopleController and create view

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PeopleController extends Controller
 {
+    public function index()
+    {
+        return view('people.index');
+    }
+    public function create()
+    {
+
+
+    }
 
 }

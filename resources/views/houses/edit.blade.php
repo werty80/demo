f7574616742599575cf78e9584db0a8e9c8b9e03
Sayim <saayim.veem@gmail.com>
2025-04-10 13:34:32 +0500

    Fix URL parameter for house ID in edit view cancel button

--- resources/views/houses/edit.blade.php
+++ resources/views/houses/edit.blade.php
@@ -36,7 +36,7 @@
                 </div>
 
                 <div class="flex items-center gap-x-6">
-                    <a href="/villages/{{ $house->id=2 }}"
+                    <a href="/villages/{{ $house->id }}"
                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Cancel</a>
                     <div>

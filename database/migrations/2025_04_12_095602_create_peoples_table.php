1223e92be1008490178c65e5dd24e5b3b1cebfb0
Sayim <saayim.veem@gmail.com>
2025-04-12 14:58:11 +0500

    Add migration to create 'peoples' table

---
+++ database/migrations/2025_04_12_095602_create_peoples_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\House;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peoples', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignIdFor(House::class);
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('address')->nullable();
+            $table->string('nationality')->nullable();
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};

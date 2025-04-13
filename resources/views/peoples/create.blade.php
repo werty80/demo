3004858073566e05d2b1b07e02d4b97a75498f1c
Sayim <saayim.veem@gmail.com>
2025-04-13 11:09:38 +0500

    Fix input tag formatting in date of birth field

--- resources/views/peoples/create.blade.php
+++ resources/views/peoples/create.blade.php
@@ -89,14 +89,14 @@
                         <label for="date_of_birth" class="block text-sm/6 font-medium text-gray-900">Date of
                             Birth</label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text"
                                        name="date_of_birth"
                                        id="date_of_birth"
                                        class="block min-w-0 grow py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
                                        value="{{ $peoples->date_of_birth }}"
-                                       required>
+                                       required
+                                >
                             </div>
                             @error('date_of_birth')
                             <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>

46ef8dbaa10dbb4ca42b1b8aebf55b69649c468c
Sayim <saayim.veem@gmail.com>
2025-04-17 11:59:31 +0500

    Refactor `VillageController` by removing redundant comments

--- app/Http/Controllers/VillageController.php
+++ app/Http/Controllers/VillageController.php
@@ -10,17 +10,11 @@ use Dflydev\DotAccessData\Data;
 
 class VillageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $islands = Island::all();
@@ -28,9 +22,6 @@ class VillageController extends Controller
         return view('village.create', compact('islands', 'selectedIsland'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Data $data)
     {
         $data = request()->validate([
@@ -43,9 +34,6 @@ class VillageController extends Controller
         return redirect()->route('islands.show', $data['island_id']);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Island $island, Village $village, House $house, People $people)
     {
         $village->load('houses');
@@ -58,17 +46,11 @@ class VillageController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Village $village)
     {
         return view('village.edit', ['village' => $village]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Village $village)
     {
         $data = request()->validate([
@@ -83,9 +65,6 @@ class VillageController extends Controller
         return redirect()->route('islands.show', $data['island_id']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Village $village)
     {
         $village->delete();
